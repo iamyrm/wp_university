0adcb0f0980571e28b4b8a00e8d896edebe98619
Yagyaraj Majhi <majhiyagya123@gmail.com>
2024-09-29 08:35:52 +0545

    Events custom post type made

--- functions.php
+++ functions.php
@@ -27,3 +27,20 @@ function university_features()
     // register_nav_menu('footerMenuLocation2','Footer Menu 2');
 }
 add_action('after_setup_theme', 'university_features');
+
+function university_adjust_query($query)
+{
+    if (!is_admin() && is_post_type_archive() && $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+        ));
+    }
+}
+add_action('pre_get_posts', 'university_adjust_query');
