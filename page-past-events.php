8f8e056daf754922b8fa2583505a2465142f2f4c
Yagyaraj Majhi <majhiyagya123@gmail.com>
2024-11-05 17:59:11 +0545

    004 Reduce Duplication - get_template_part()

--- page-past-events.php
+++ page-past-events.php
@@ -1,8 +1,8 @@
-<?php get_header(); 
+<?php get_header();
 pageBanner(array(
-  'title'=>'Past Events',
-  'subtitle'=>'Recap of our past events.',
-  ));?>
+  'title' => 'Past Events',
+  'subtitle' => 'Recap of our past events.',
+)); ?>
 
 <div class="container container--narrow page-section">
   <?php
@@ -26,28 +26,7 @@ pageBanner(array(
 
   while ($pastEvents->have_posts()) {
     $pastEvents->the_post();
-  ?>
-    <div class="event-summary">
-      <a class="event-summary__date t-center" href="#">
-        <span class="event-summary__month">
-          <?php
-          $eventDate = new DateTime(get_field('event_date'));
-          echo $eventDate->format('M');
-          ?>
-        </span>
-        <span class="event-summary__day">
-          <?php echo $eventDate->format('d'); ?>
-        </span>
-      </a>
-      <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-        <p>
-          <?php echo wp_trim_words(get_the_content(), 18) ?>
-          <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a>
-        </p>
-      </div>
-    </div>
-  <?php
+    get_template_part('parts/event'); 
   }
   echo paginate_links(array(
     'total' => $pastEvents->max_num_pages,
