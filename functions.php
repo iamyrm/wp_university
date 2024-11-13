5ffdf135deee815ad84cd7dd85605513375be865
Yagyaraj Majhi <majhiyagya123@gmail.com>
2024-11-13 20:19:12 +0545

    002 Getting Our Computer & Project Ready for JavaScript

--- functions.php
+++ functions.php
@@ -40,15 +40,15 @@ function load_scripts()
 {
     wp_enqueue_style('main_css', get_stylesheet_uri());
 
-    wp_enqueue_style('site_css', get_theme_file_uri('/assets/build/style-index.css'));
+    wp_enqueue_style('site_css', get_theme_file_uri('/build/style-index.css'));
 
-    wp_enqueue_style('site_extra_css', get_theme_file_uri('/assets/build/index.css'));
+    wp_enqueue_style('site_extra_css', get_theme_file_uri('/build/index.css'));
 
     wp_enqueue_style('font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
     wp_enqueue_style('google_fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 
-    wp_enqueue_script('main_js', get_theme_file_uri('/assets/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('main_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
 
